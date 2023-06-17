e4b48da8dcad7a5873656ba248a0d9e871451a6f
3v0k4 <riccardo.odone@gmail.com>
2023-06-17 17:00:55 +0200

    init

---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+})
