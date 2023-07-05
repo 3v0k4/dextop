c83a39b15e5d7ab236d9730bd5e86402f0ae15cc
3v0k4 <riccardo.odone@gmail.com>
2023-07-05 21:50:56 +0200

    style(website): tweaks

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'onest': ['Onest', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
